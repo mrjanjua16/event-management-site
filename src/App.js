f671198d01be7a8c70c28b127b6406e96387a200
Mubashir Ahmad <imubashirahmad@gmail.com>
2024-05-17 15:57:01 +0000

    Registration section added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.scss';
 import Navbar from './components/Navbar';
 import Banner1 from './components/Banner1';
+import About1 from './components/About1';
+import Speaker from './components/Speaker';
+import Event from './components/Event';
+import Registration from './components/Registration';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Banner1 />
+      <About1 />
+      <Speaker />
+      <Event />
+      <Registration />
     </div>
   );
 }

b26bff0f766f54b13c5061af32a1aaa608991c05
Mubashir Ahmad <imubashirahmad@gmail.com>
2024-06-01 13:21:32 +0000

    navbar fixes

--- src/components/Main/Navbar.js
+++ src/components/Main/Navbar.js
@@ -1,4 +1,4 @@
-
+import React, { useState } from 'react';
 import '../../scss/components/_navigation.scss';
 import '../../scss/components/_common.scss';
 import "../../scss/_typography.scss";
@@ -8,105 +8,115 @@ import logo from '../../images/logo.png';
 import ticket from '../../images/icon/ticket.png';
 import { Link } from 'react-router-dom';
 
+export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
 
-export default function Navbar()
-{
+    const toggleNavbar = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-<>
-<nav class="navbar main-nav border-less fixed-top navbar-expand-lg p-0">
-  <div class="container-fluid p-0">
-    {
-    // logo
-    } 
-    <Link class="navbar-brand" to="/">
-      <img src={logo} alt="logo" />
-    </Link>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="fa fa-bars"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav mx-auto">
-        <li class="nav-item dropdown active">
-          <Link class="nav-link" to="/" data-toggle="dropdown">Home <i class="fa fa-angle-down"></i>
-            <span>/</span>
-          </Link>
-          {
-          // Dropdown list 
-          }
-          <ul class="dropdown-menu">
-            <li><Link class="dropdown-item" to="/">Homepage</Link></li>
-            <li><Link class="dropdown-item" to="/Home2">Homepage 2</Link></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <Link class="nav-link" to="/Speaker">Speakers
-            <span>/</span>
-          </Link>
-        </li>
-        <li class="nav-item dropdown">
-          <Link class="nav-link" to="/About" data-toggle="dropdown">Pages <i class="fa fa-angle-down"></i><span>/</span></Link>
-          {
-          //<!-- Dropdown list -->
-            }
-          <ul class="dropdown-menu">
-            <li><Link class="dropdown-item" to="/About">About Us</Link></li>
-            <li><Link class="dropdown-item" to="/SpeakerDetail">Single Speaker</Link></li>
-            <li><Link class="dropdown-item" to="/GalleryDetail">Gallery</Link></li>
-            <li><Link class="dropdown-item" to="/GalleryDetail2">Gallery-02</Link></li>
-            <li><Link class="dropdown-item" to="/Testimonal">Testimonial</Link></li>
-            <li><Link class="dropdown-item" to="/Price">Pricing</Link></li>
-            <li><Link class="dropdown-item" to="/FAQ">FAQ</Link></li>
-            <li><Link class="dropdown-item" to="/Error">404</Link></li>
-                    
-            <li class="dropdown dropdown-submenu dropright">
-              <Link class="dropdown-item dropdown-toggle" to="/" id="dropdown0301" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sub Menu</Link>
-    
-              <ul class="dropdown-menu" aria-labelledby="dropdown0301">
-                <li><Link class="dropdown-item" to="/">Submenu 01</Link></li>
-                <li><Link class="dropdown-item" to="/Home2">Submenu 02</Link></li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <Link class="nav-link" to="/Schedule">Schedule<span>/</span></Link>
-        </li>
-        <li class="nav-item">
-          <Link class="nav-link" to="/Sponsors">Sponsors<span>/</span></Link>
-        </li>
-        <li class="nav-item dropdown">
-          <Link class="nav-link" to="/NewsWithoutSlider" data-toggle="dropdown">News <i class="fa fa-angle-down"></i><span>/</span>
-          </Link>
-          {
-          //<!-- Dropdown list -->
-            }
-          <ul class="dropdown-menu">
-            <li><Link class="dropdown-item" to="/NewsWithoutSlider">News without sidebar</Link></li>
-            <li><Link class="dropdown-item" to="/NewsWithRightSlider">News with right sidebar</Link></li>
-            <li><Link class="dropdown-item" to="/NewsWithLeftSlider">News with left sidebar</Link></li>
-            <li><Link class="dropdown-item" to="/NewsSinglePage">News Single</Link></li>
-
-            <li class="dropdown dropdown-submenu dropleft">
-              <Link class="dropdown-item dropdown-toggle" to="/" id="dropdown0501" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sub Menu</Link>
-    
-              <ul class="dropdown-menu" aria-labelledby="dropdown0501">
-                <li><Link class="dropdown-item" to="/">Submenu 01</Link></li>
-                <li><Link class="dropdown-item" to="/Home2">Submenu 02</Link></li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <Link class="nav-link" to="/Contact">Contact</Link>
-        </li>
-      </ul>
-      <Link to="/Contact" class="ticket">
-        <img src={ticket} alt="ticket" />
-        <span>Buy Ticket</span>
-      </Link>
-    </div>
-  </div>
-</nav>
-</>
-);
+        <>
+            <nav className="navbar main-nav border-less fixed-top navbar-expand-lg p-0">
+                <div className="container-fluid p-0">
+                    {/* Logo */}
+                    <Link className="navbar-brand" to="/">
+                        <img src={logo} alt="logo" />
+                    </Link>
+                    <button 
+                        className="navbar-toggler" 
+                        type="button" 
+                        aria-controls="navbarNav" 
+                        aria-expanded={isOpen} 
+                        aria-label="Toggle navigation" 
+                        onClick={toggleNavbar}
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`} id="navbarNav">
+                        <ul className="navbar-nav mx-auto">
+                            {/* Home Dropdown */}
+                            <li className="nav-item dropdown active">
+                                <Link className="nav-link dropdown-toggle" to="/" id="homeDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Home <i className="fa fa-angle-down"></i><span>/</span>
+                                </Link>
+                                <ul className="dropdown-menu" aria-labelledby="homeDropdown">
+                                    <li><Link className="dropdown-item" to="/">Homepage</Link></li>
+                                    <li><Link className="dropdown-item" to="/Home2">Homepage 2</Link></li>
+                                </ul>
+                            </li>
+                            {/* Speakers Link */}
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/Speaker">Speakers<span>/</span></Link>
+                            </li>
+                            {/* Pages Dropdown */}
+                            <li className="nav-item dropdown">
+                                <Link className="nav-link dropdown-toggle" to="/About" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Pages <i className="fa fa-angle-down"></i><span>/</span>
+                                </Link>
+                                <ul className="dropdown-menu" aria-labelledby="pagesDropdown">
+                                    <li><Link className="dropdown-item" to="/About">About Us</Link></li>
+                                    <li><Link className="dropdown-item" to="/SpeakerDetail">Single Speaker</Link></li>
+                                    <li><Link className="dropdown-item" to="/GalleryDetail">Gallery</Link></li>
+                                    <li><Link className="dropdown-item" to="/GalleryDetail2">Gallery-02</Link></li>
+                                    <li><Link className="dropdown-item" to="/Testimonal">Testimonial</Link></li>
+                                    <li><Link className="dropdown-item" to="/Price">Pricing</Link></li>
+                                    <li><Link className="dropdown-item" to="/FAQ">FAQ</Link></li>
+                                    <li><Link className="dropdown-item" to="/Error">404</Link></li>
+                                    {/* Submenu */}
+                                    <li className="dropdown dropdown-submenu dropright">
+                                        <Link className="dropdown-item dropdown-toggle" to="/" id="submenuDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            Sub Menu
+                                        </Link>
+                                        <ul className="dropdown-menu" aria-labelledby="submenuDropdown">
+                                            <li><Link className="dropdown-item" to="/">Submenu 01</Link></li>
+                                            <li><Link className="dropdown-item" to="/Home2">Submenu 02</Link></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                            {/* Schedule Link */}
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/Schedule">Schedule<span>/</span></Link>
+                            </li>
+                            {/* Sponsors Link */}
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/Sponsors">Sponsors<span>/</span></Link>
+                            </li>
+                            {/* News Dropdown */}
+                            <li className="nav-item dropdown">
+                                <Link className="nav-link dropdown-toggle" to="/NewsWithoutSlider" id="newsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    News <i className="fa fa-angle-down"></i><span>/</span>
+                                </Link>
+                                <ul className="dropdown-menu" aria-labelledby="newsDropdown">
+                                    <li><Link className="dropdown-item" to="/NewsWithoutSlider">News without sidebar</Link></li>
+                                    <li><Link className="dropdown-item" to="/NewsWithRightSlider">News with right sidebar</Link></li>
+                                    <li><Link className="dropdown-item" to="/NewsWithLeftSlider">News with left sidebar</Link></li>
+                                    <li><Link className="dropdown-item" to="/NewsSinglePage">News Single</Link></li>
+                                    {/* Submenu */}
+                                    <li className="dropdown dropdown-submenu dropleft">
+                                        <Link className="dropdown-item dropdown-toggle" to="/" id="newsSubmenuDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            Sub Menu
+                                        </Link>
+                                        <ul className="dropdown-menu" aria-labelledby="newsSubmenuDropdown">
+                                            <li><Link className="dropdown-item" to="/">Submenu 01</Link></li>
+                                            <li><Link className="dropdown-item" to="/Home2">Submenu 02</Link></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                            {/* Contact Link */}
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/Contact">Contact</Link>
+                            </li>
+                        </ul>
+                        <Link to="/Contact" className="ticket">
+                            <img src={ticket} alt="ticket" />
+                            <span>Buy Ticket</span>
+                        </Link>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
 }
